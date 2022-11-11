42d8cf4617c97355f4b63d7878ffcc3b174b2299
myuri001 <63941768+myuri001@users.noreply.github.com>
2022-11-11 15:47:37 +0600

    создал файл krysha.py для парсинга сайта

---
+++ krysha.py
@@ -0,0 +1,15 @@
+import requests
+
+headers = {
+    'text': '/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+}
+def get_page(url):
+    pass
+
+
+def main():
+    get_page(url="")
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs
+from pprint import pprint
 
-def foo():
-    pass
+number_id = {}
+reponse = urlopen('https://krisha.kz/arenda/kvartiry/almaty/?areas=&das[_sys.hasphoto]=1&das[live.rooms]=1&das[price][from]=150000&das[price][to]=250000&das[rent.period]=2&das[who]=1&lat=43.23621&lon=76.92370&zoom=14')
+html = reponse.read().decode('utf-8')
+soup = bs(html)
+for link in soup.find_all('a'):
+    if link.has_attr('href'):
+        s= link.get('href')
+        if s.startswith('/a/show/'):
+            number_id.setdefault(int(s.split('/')[3]))
+
+pprint(number_id)
